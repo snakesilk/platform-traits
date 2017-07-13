b5917c9022912221f57e56e49647adfe4c5985a3
Pontus Persson <pom@skurt.com>
2017-07-13 16:13:27 +0200

    Build in ./dist

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,10 @@
 #! /usr/bin/env bash
 set -e
 
-DIST="./package"
+DIST="./dist"
 
 if [ -d "$DIST" ]; then
     rm -r $DIST
 fi
 
-babel ./src --out-dir $DIST/lib
-
-cp package.json $DIST
+babel ./src --out-dir $DIST
