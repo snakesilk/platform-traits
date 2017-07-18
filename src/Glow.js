805e7b70f6831ea2af7cbeda7bc92ec6f7d484a7
Pontus Persson <pontus.alexander@gmail.com>
2017-07-18 10:27:10 +0200

    Refactor Light trait.

--- src/Glow.js
+++ src/Glow.js
@@ -7,22 +7,6 @@ class Glow extends Light
         super();
         this.NAME = 'glow';
     }
-    __attach(host)
-    {
-        super.__attach(host);
-        const model = this._host.model;
-        this.lamps.forEach(lamp => {
-            model.add(lamp.light);
-        });
-    }
-    __detach()
-    {
-        const model = this._host.model;
-        this.lamps.forEach(lamp => {
-            model.remove(lamp.light);
-        });
-        super.__detach();
-    }
 }
 
 module.exports = Glow;
